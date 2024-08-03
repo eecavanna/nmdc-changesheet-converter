ab4ea186c9379103c61d4270735e4e162b51638e
eecavanna <eecavanna@users.noreply.github.com>
2024-08-02 22:46:23 -0700

    Install `react-data-grid` NPM package and use it to display data grid

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nmdc-changesheet-converter
 
 - [papaparse](https://github.com/mholt/PapaParse) - CSV parser that runs in the browser
+- [react-data-grid](https://github.com/adazzle/react-data-grid/) - Displays tabular data as a grid
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { parse } from "papaparse";
-import "./App.css";
+import DataGrid from "react-data-grid";
+import "react-data-grid/lib/styles.css";
 
 const csvStr = `
 product,price
@@ -16,8 +17,36 @@ jello
 `.trim();
 
 function App() {
-  const parseResult = parse<string[][]>(csvStr, { skipEmptyLines: true });
-  return <>{JSON.stringify(parseResult.data)}</>;
+  const parseResult = parse<string[]>(csvStr, { skipEmptyLines: true });
+
+  // Get column names from first parsed row.
+  const columns = parseResult.data[0].map((columnName) => ({
+    key: columnName,
+    name: columnName,
+  }));
+
+  // Get data row content from the second parsed row and onward.
+  const rows = parseResult.data.slice(1).map((parsedRow) => {
+    const row: Record<string, string> = {};
+    columns.forEach((column, index) => {
+      row[column.key] = parsedRow[index];
+    });
+    return row;
+  });
+
+  return (
+    <>
+      <div style={{ marginBottom: 8, padding: 8, border: "1px solid yellow" }}>
+        <pre>{csvStr}</pre>
+      </div>
+      <div style={{ marginBottom: 8, padding: 8, border: "1px solid green" }}>
+        <code>{JSON.stringify(parseResult.data)}</code>
+      </div>
+      <div style={{ marginBottom: 8 }}>
+        <DataGrid columns={columns} rows={rows} />
+      </div>
+    </>
+  );
 }
 
 export default App;
